b3ccd56f06e4f6eafd112e83775e89e5e34cca07
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-05-15 15:36:57 -0300

    rate limiter with redis

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
@@ -16,6 +17,8 @@ import "@shared/container";
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
